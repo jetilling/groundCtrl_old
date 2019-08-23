5cfe64d94bdf1fd56023a0d614dd4cda6754b0ac
jetilling <jetilling@hotmail.com>
2019-08-23 15:33:37 -0400

    users can auto-update payment amount

--- resources/js/payment_calculator/components/app.js
+++ resources/js/payment_calculator/components/app.js
@@ -8,6 +8,7 @@ export default class App extends Component {
       workspaces: [],
       totalAmount: 0,
       totalHours: 0,
+      previousTotalHours: 0,
       averageHourAmount: 0
     };
 
@@ -23,21 +24,24 @@ export default class App extends Component {
     let parsedHours;
     switch(stateName) {
       case 'hours':
-        parsedHours = parseInt(event.target.value)
-        this.updateStateHoursAndAmounts(hourlyRate, parsedHours)
+        parsedHours = parseInt(event.target.value) ? parseInt(event.target.value) : 0
+        this.updateStateHoursAndAmounts(hourlyRate, parsedHours, isDelete)
         break;
       case 'minutes':
-        parsedHours = parseInt(event.target.value) / 60
-        this.updateStateHoursAndAmounts(hourlyRate, parsedHours)
+        parsedHours = parseInt(event.target.value) ? parseInt(event.target.value) / 60 : 0
+        this.updateStateHoursAndAmounts(hourlyRate, parsedHours, isDelete)
         break;
     }
   }
 
-  updateStateHoursAndAmounts(hourlyRate, parsedHours) {
-    let totalHours = this.state.totalHours + parsedHours
-    let totalAmount = parseFloat(this.state.totalAmount) + (hourlyRate * parsedHours)
+  updateStateHoursAndAmounts(hourlyRate, parsedHours, isDelete) {
+    console.log(parsedHours)
+    let previousTotalHours = parsedHours;
+    let totalHours = this.state.totalHours + (parsedHours - this.state.previousTotalHours)
+    let totalAmount = parseFloat(this.state.totalAmount) + (hourlyRate * (parsedHours - this.state.previousTotalHours))
     let averageHourAmount = totalAmount / totalHours
     let newStateElements = {
+      previousTotalHours: previousTotalHours,
       totalHours: totalHours,
       totalAmount: totalAmount.toFixed(2),
       averageHourAmount: averageHourAmount.toFixed(2)
@@ -50,49 +54,56 @@ export default class App extends Component {
     let inputStyle = {
       width: "40%"
     }
-    let workspaceElements = this.state.workspaces.map(workspace => {
-      return <div className="box" key={workspace.name}>
-          <div className="columns">
-            <div className="column is-half">
-              <h6 className="title is-4">{workspace.name}</h6>
-            </div>
-            <div className="column is-half">
-              <div className="columns">
-                <div className="column is-two-fifth is-offset-three-fifths">
-                  <h6 className="title is-4">${parseInt(workspace.hourly_rate)}</h6>
-                </div>
+    let workspaceElements = <div>No Workspaces Loaded</div>;
+    
+    if (this.state.workspaces.length) {
+      workspaceElements = this.state.workspaces.map(workspace => {
+        return <div className="box" key={workspace.name}>
+            <div className="columns">
+              <div className="column is-half">
+                <h6 className="title is-4">{workspace.name}</h6>
               </div>
-            </div>
-            
-          </div>
-          <div className="columns">
-            <div className="column is-two-fifths">
-                <div className="field">
-                  <label className="label">Hours</label>
-                  <div className="control">
-                    <input 
-                      className="input" 
-                      style={inputStyle} 
-                      name="hours"
-                      onChange={this.handleInputChange.bind(null, 'hours', workspace.hourly_rate)} />
+              <div className="column is-half">
+                <div className="columns">
+                  <div className="column is-two-fifth is-offset-three-fifths">
+                    <h6 className="title is-4">${parseInt(workspace.hourly_rate)}</h6>
                   </div>
                 </div>
               </div>
+              
+            </div>
+            <div className="columns">
               <div className="column is-two-fifths">
-                <div className="field">
-                  <label className="label">Minutes</label>
-                  <div className="control">
-                    <input 
-                      className="input" 
-                      style={inputStyle} 
-                      name="minutes"
-                      onChange={this.handleInputChange.bind(null, 'minutes', workspace.hourly_rate)} />
+                  <div className="field">
+                    <label className="label">Hours</label>
+                    <div className="control">
+                      <input 
+                        className="input" 
+                        style={inputStyle} 
+                        name="hours"
+                        onChange={this.handleInputChange.bind(null, 'hours', workspace.hourly_rate)} />
+                    </div>
                   </div>
                 </div>
-              </div>
-          </div>
-      </div>
-    })
+                <div className="column is-two-fifths">
+                  <div className="field">
+                    <label className="label">Minutes</label>
+                    <div className="control">
+                      <input 
+                        className="input" 
+                        style={inputStyle} 
+                        name="minutes"
+                        onChange={this.handleInputChange.bind(null, 'minutes', workspace.hourly_rate)} />
+                    </div>
+                  </div>
+                </div>
+                <div className="level is-one-fifths">
+                  <button className="button is-small">Update</button>
+                </div>
+            </div>
+        </div>
+      })
+    }
 
     return <div>
       <div className="columns height-80vh">
--- resources/js/payment_calculator/index.js
+++ resources/js/payment_calculator/index.js
@@ -4,8 +4,13 @@ import '../../styles/index.css';
 import App from './components/app'
 import * as serviceWorker from './serviceWorker';
 
+let workspaceArray = []
+for (let key in window.workspaces) {
+  workspaceArray.push(window.workspaces[key])
+}
+
 ReactDOM.render(
-  <App workspaces={window.workspaces} />,
+  <App workspaces={workspaceArray} />,
  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
